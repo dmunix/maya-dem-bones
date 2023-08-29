430d20e39da9b21f6b7f581fffadf2845f8ba5f1
LAPTOP-QP382TKM\munix <davide@genies.com>
2023-08-29 10:49:42 -0700

    debug

--- modules/FindMayaPython.cmake
+++ modules/FindMayaPython.cmake
@@ -124,6 +124,12 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(MayaPython
     REQUIRED_VARS Python_EXECUTABLE Python_INCLUDE_DIR Python_LIBRARY)
 
+message(STATUS "Maya Python_LIBRARY: ${Python_LIBRARY}")
+message(STATUS "Maya Python_INCLUDE_DIR: ${Python_INCLUDE_DIR}")
+message(STATUS "Maya Python_EXECUTABLE: ${Python_EXECUTABLE}")
+set(Python_LIBRARY /usr/autodesk/maya2022/lib/libpython3.so)
+set(Python_INCLUDE_DIR /usr/autodesk/maya2022/include/Python37/Python)
+set(Python_EXECUTABLE /usr/autodesk/maya2022/mayapy)
 message(STATUS "Searching for python....")
 find_package(Python REQUIRED COMPONENTS Interpreter Development)
 message(STATUS "Searching for python....DONE")
