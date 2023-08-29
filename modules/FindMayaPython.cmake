47f5b17360e6cb1dab666d75794f3f7fabda3308
LAPTOP-QP382TKM\munix <davide@genies.com>
2023-08-29 11:42:04 -0700

    debug

--- modules/FindMayaPython.cmake
+++ modules/FindMayaPython.cmake
@@ -114,7 +114,7 @@ message(STATUS "Maya Python_EXECUTABLE: ${Python_EXECUTABLE}")
 
 set(Python_LIBRARY /usr/autodesk/maya2022/lib/libpython3.so)
 set(Python_INCLUDE_DIR /usr/autodesk/maya2022/include/Python37/Python)
-set(Python_EXECUTABLE /usr/autodesk/maya2022/mayapy)
+set(Python_EXECUTABLE /usr/autodesk/maya2022/bin/mayapy)
 
 message(STATUS "Maya Python_LIBRARY: ${Python_LIBRARY}")
 message(STATUS "Maya Python_INCLUDE_DIR: ${Python_INCLUDE_DIR}")
@@ -127,9 +127,11 @@ find_package_handle_standard_args(MayaPython
 message(STATUS "Maya Python_LIBRARY: ${Python_LIBRARY}")
 message(STATUS "Maya Python_INCLUDE_DIR: ${Python_INCLUDE_DIR}")
 message(STATUS "Maya Python_EXECUTABLE: ${Python_EXECUTABLE}")
+
 set(Python_LIBRARY /usr/autodesk/maya2022/lib/libpython3.so)
 set(Python_INCLUDE_DIR /usr/autodesk/maya2022/include/Python37/Python)
-set(Python_EXECUTABLE /usr/autodesk/maya2022/mayapy)
+set(Python_EXECUTABLE /usr/autodesk/maya2022/bin/mayapy)
+
 message(STATUS "Searching for python....")
 find_package(Python REQUIRED COMPONENTS Interpreter Development)
 message(STATUS "Searching for python....DONE")
